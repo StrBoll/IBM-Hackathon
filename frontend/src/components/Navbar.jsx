6faee4ccd445847dd42fc3380d0f7933907e5bcd
erobx <erob7856@gmail.com>
2024-10-26 15:36:39 -0400

    more things

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+    </>
+  )
+}
+
+export default Navbar;
