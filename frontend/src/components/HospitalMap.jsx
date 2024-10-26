b3137b4a604031aeee7686cf4ddeaad07365b55a
darango3002 <donaldarango@ufl.edu>
2024-10-26 16:58:42 -0400

    added hospital map

--- frontend/src/components/HospitalMap.jsx
+++ frontend/src/components/HospitalMap.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
 import L from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 
 function HospitalMap() {
 
-  var geojson = {
+  var geoJsonFeature = {
     "type": "FeatureCollection",
     "name": "ep122024-020_5day_lin",
     "features": [
@@ -12,19 +12,13 @@ function HospitalMap() {
     ]
     }
 
-   
-
-  return (
-    <MapContainer className='map' center={[29.651634, -82.324829]} zoom={13} >
+  return (  
+    <MapContainer id='map' style={{height: '100vh'}} center={[29.651634, -82.324829]} zoom={13} >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
         url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
       />
-      <Marker position={[29.651634, -82.324829]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <GeoJSON data={geoJsonFeature} />
   </MapContainer>
   );
 }
