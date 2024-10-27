ed0fbe70c49297ef9c0cf8631d6566ce1aa03715
darango3002 <donaldarango@ufl.edu>
2024-10-27 04:24:21 -0400

    added hosptial icons and hurricane path on map

--- backend/main.py
+++ backend/main.py
@@ -23,9 +23,9 @@ def index():
     return {"Hello": "world!"}
 
 @app.get("/api/hurricane/{storm_id}")
-async def get_hurricane_geojson(storm_id: str):
+async def get_hurricane_geojson(storm_id: str, type: str = "pgn"):
 
-    url = f"https://www.nhc.noaa.gov/gis/forecast/archive/{storm_id}_5day_001.zip"
+    url = f"https://www.nhc.noaa.gov/gis/forecast/archive/{storm_id}_5day_001.zip"  
 
     try:
         response = requests.get(url)
@@ -38,15 +38,12 @@ async def get_hurricane_geojson(storm_id: str):
         zip_ref.extractall("temp_shapefiles")
 
     # Find the shapefile (.shp file) path
-    shapefile_path1 = None
+    shapefile_path = None
     for root, dirs, files in os.walk("temp_shapefiles"):
         for file in files:
-            if file.endswith("pts.shp"):
+            if file.endswith(f'{type}.shp'):
                 shapefile_path = os.path.join(root, file)
                 break
-
-    if shapefile_path is None:
-        raise HTTPException(status_code=400, detail="Shapefile not found in the ZIP file")
 
     # Step 3: Load shapefiles using GeoPandas and convert to GeoJSON
     try:
@@ -66,51 +63,3 @@ async def get_hurricane_geojson(storm_id: str):
 
     # Return GeoJSON object
     return geojson_obj
-
-@app.get("/api/hurricanetrack/{storm_id}")
-async def get_hurricane_geojson(storm_id: str):
-
-    url = f"https://www.nhc.noaa.gov/gis/best_track/{storm_id}_best_track.zip"
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        raise HTTPException(status_code=400, detail="Error downloading zip file") from e
-
-    # Step 2: Extract the ZIP file
-    with zipfile.ZipFile(BytesIO(response.content)) as zip_ref:
-        zip_ref.extractall("temp_shapefiles")
-
-    # Find the shapefile (.shp file) path
-    shapefile_path1 = None
-    for root, dirs, files in os.walk("temp_shapefiles"):
-        for file in files:
-            if file.endswith(".shp"):
-                shapefile_path = os.path.join(root, file)
-                break
-
-    if shapefile_path is None:
-        raise HTTPException(status_code=400, detail="Shapefile not found in the ZIP file")
-
-    # Step 3: Load shapefiles using GeoPandas and convert to GeoJSON
-    try:
-        gdf = gpd.read_file(shapefile_path)
-        geojson_obj = gdf.to_json()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Error converting shapefile to GeoJSON") from e
-    finally:
-        # Cleanup extracted files
-        for root, dirs, files in os.walk("temp_shapefiles", topdown=False):
-            for file in files:
-                os.remove(os.path.join(root, file))
-            for dir in dirs:
-                os.rmdir(os.path.join(root, dir))
-        os.rmdir("temp_shapefiles")
-
-    # Return GeoJSON object
-    return geojson_obj
-
-
-
-
--- frontend/src/components/HospitalMap.jsx
+++ frontend/src/components/HospitalMap.jsx
@@ -1,26 +1,190 @@
-import React, { Component } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
-import L from 'leaflet';
+import React, { Component, useState, useEffect, useRef } from 'react';
+import { MapContainer, TileLayer, GeoJSON, useMap, Marker, Popup } from 'react-leaflet';
+import axios from 'axios';
+import L, { latLng, map, point } from 'leaflet';
+import hospitals from '../assets/hospitals.json'
+import hospitalPng from '../assets/hospital.png'
+import { getDistanceFromLatLonInMiles } from '../helper/helper';
+
+function HospitalIcons(hospitalNearestPoints) {
+
+  const hospitalIcon = L.icon({
+    iconUrl: hospitalPng,
+    iconSize: [20, 20],
+    iconAnchor: [10, 10],
+    popupAnchor: [0, -20],
+  });
+
+  const map = useMap('map');
+  const markersRef = useRef({});
+
+  useEffect(() => {
+    console.log('hospitalNearestPointsFUNC:', hospitalNearestPoints);
+
+    if (hospitalNearestPoints.length < 0) return;
+
+    hospitals.forEach((data, index) => {
+      const position = latLng(data.latitude, data.longitude);
+      const popupContent = (
+        `<div>
+          <h2>${data.name}</h2>
+          <p>Address: ${data.address}</p>
+          <p>Distance to closest point: ${!!hospitalNearestPoints[index] ? hospitalNearestPoints[index].distance.toFixed(2) : 'N/A'} miles</p>
+          <p>Category: ${!!hospitalNearestPoints[index] ? hospitalNearestPoints[index].point.category : 'N/A'}</p>
+        </div>`
+      )
+
+      if (markersRef.current[index]) {
+        // If marker exists, update position and popup content  
+        markersRef.current[index].setLatLng(position).setPopupContent(popupContent);
+      } else {
+        // Create a new marker if it doesn't already exist
+        const newMarker = L.marker(position).addTo(map).bindPopup(popupContent).setIcon(hospitalIcon);
+        markersRef.current[index] = newMarker;
+      }
+    })
+  }, [hospitalNearestPoints, map]);
+
+  return( null
+    // hospitals.map((hospital, index) => (
+    //   <Marker 
+    //     key={index}
+    //     position={[hospital.latitude, hospital.longitude]}
+    //     icon={hospitalIcon}
+    //   >
+    //     <Popup>
+    //       <div>
+    //         <h2>{hospital.name}</h2>
+    //         <p>Address: {hospital.address}</p>
+    //         <p>Distance to closest point: {hospitalNearestPoints[index] ? hospitalNearestPoints[index].distance.toFixed(2) : 'N/A'} miles</p>
+    //         <p>Category: {hospitalNearestPoints[index] ? hospitalNearestPoints[index].point.category : 'N/A'}</p>
+    //       </div>
+    //     </Popup>
+    //   </Marker>
+    // ))
+  )
+  
+}
 
 function HospitalMap() {
 
-  var geoJsonFeature = {
-    "type": "FeatureCollection",
-    "name": "ep122024-020_5day_lin",
-    "features": [
-        { "type": "Feature", "properties": { "STORMNAME": "Kristy", "STORMTYPE": "HU", "ADVDATE": "800 AM PDT Sat Oct 26 2024", "ADVISNUM": "20", "STORMNUM": 12.0, "FCSTPRD": 120.0, "BASIN": "EP" }, "geometry": { "type": "LineString", "coordinates": [ [ -128.4, 19.7 ], [ -129.2, 21.1 ], [ -130.0, 22.3 ], [ -131.0, 22.5 ], [ -132.2, 21.9 ] ] } }
-    ]
-    }
+  const [pointsGEO, setPointsGEO] = useState(null)
+  const [projectionGEO, setProjectionGEO] = useState(null)
+  const [lineGEO, setLineGEO] = useState(null)
 
+  const pointsList = []
+  const [hospitalNearestPoints, setHospitalNearestPoints] = useState([])
+
+  const hurricaneID = 'al142024' // Milton
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/hurricane/${hurricaneID}?type=pts`)
+      .then(response => {
+        setPointsGEO(JSON.parse(response.data));
+        console.log('response:', JSON.parse(response.data))
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }, []);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/hurricane/${hurricaneID}?type=pgn`)
+      .then(response => {
+        setProjectionGEO(JSON.parse(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/hurricane/${hurricaneID}?type=lin`)
+      .then(response => {
+        setLineGEO(JSON.parse(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!pointsGEO) return;
+
+    for (let i = 0; i < pointsGEO.features.length; i++) {
+      const point = {
+        id: pointsGEO.features[i].id,
+        lat: pointsGEO.features[i].geometry.coordinates[1],
+        lng: pointsGEO.features[i].geometry.coordinates[0],
+        category: pointsGEO.features[i].properties['SSNUM'],
+      }
+      pointsList.push(point);
+    }
+    
+    for (let i = 0; i < hospitals.length; i++) {
+      const hLat = hospitals[i].latitude;
+      const hLong = hospitals[i].longitude;
+
+      var closestPoint = [Infinity, -1];
+      pointsList.forEach((point, index) => {
+        const distance = getDistanceFromLatLonInMiles(hLat, hLong, point.lat, point.lng);
+        if (distance < closestPoint[0]) {
+          closestPoint = [distance, index];
+        }
+      });
+
+      if (closestPoint[1] === -1) {
+        console.log('No closest point found');
+        continue;
+      }
+
+      if (closestPoint[0] > 250) { // discard hospitals that are 300 miles away from the closest point
+        console.log(`Closest point for ${hospitals[i].name} is too far away`);
+        continue;
+      }
+
+      // hospitalNearestPoints.push({
+      //   hospital: hospitals[i],
+      //   hospitalIndex: i,
+      //   point: pointsList[closestPoint[1]],
+      //   distance: closestPoint[0],
+      // });
+
+      setHospitalNearestPoints(prevState => [...prevState, {
+        hospital: hospitals[i],
+        hospitalIndex: i,
+        point: pointsList[closestPoint[1]],
+        distance: closestPoint[0],
+      }]);
+    }
+    console.log('hospitalNearestPoints:', hospitalNearestPoints);
+  }, [pointsGEO]);
+    
   return (  
-    <MapContainer id='map' style={{height: '100vh'}} center={[29.651634, -82.324829]} zoom={13} >
+    <>
+    {<MapContainer 
+      whenReady={() => {
+      console.log('points', pointsGEO)
+    }} 
+      id='map' style={{height: '100vh'}} center={[29.651634, -82.324829]} zoom={10} >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}"
+        minZoom={0} 
+	      maxZoom= {10}
+        ext='png'
       />
-      <GeoJSON data={geoJsonFeature} />
-  </MapContainer>
-  );
+      {
+        <HospitalIcons hospitalNearestPoints={hospitalNearestPoints}/>
+      }
+      {pointsGEO && <GeoJSON data={pointsGEO} key={JSON.stringify(pointsGEO)} interactive={false}/>}
+      {projectionGEO && <GeoJSON data={projectionGEO} key={JSON.stringify(projectionGEO)} interactive={false}/>}
+      {lineGEO && <GeoJSON data={lineGEO} key={JSON.stringify(lineGEO)} interactive={false}/>}
+    </MapContainer> }
+    </>
+    
+  ) 
 }
 
-export default HospitalMap;
+export default HospitalMap; 
