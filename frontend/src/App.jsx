77b0098b690a8ca481777e2544fa9556e41427f3
StrBoll <phillipboll107@gmail.com>
2024-10-27 01:04:31 -0400

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,30 +3,24 @@ import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
 import HospitalMap from './components/HospitalMap';
+import SOS from './pages/SOS'
 
 const App = () => {
   return (
-    < Router >
-      <div className="app-container">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route
-              path="/"
-              element={<HomePage />}
-            />
-            <Route
-              path="/map"
-              element={<HospitalMap />}
-            />
-            <Route
-              path="/dashboard"
-              element={<Dashboard />}
-            />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <Router>
+  <div className="app-container">
+    <Navbar />
+    <main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/map" element={<HospitalMap />} />
+        <Route path="/SOS" element={<SOS />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </main>
+  </div>
+</Router>
+
   );
 }
 
